01009d1f0c72a7b08a71bd2014115103556eb39d
Luka Vujnovac <84441240+lukavujnovac@users.noreply.github.com>
2023-12-23 11:34:09 +0100

    Initial Commit

---
+++ Showcase/ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowcaseApp.swift
+//  Showcase
+//
+//  Created by Luka Vujnovac on 23.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
